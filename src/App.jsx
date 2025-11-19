efff1fb6a7347e98b7a212612c5457bb223790c5
idlakshan <id.lakshan21@gmail.com>
2025-11-19 09:36:05 +0530

    feat: add header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
-
   return (
-    <main>
-     <h1 className='text-red-500'>Hello</h1>
-    </main>
-  )
+    <>
+      <Header />
+      <main className="min-h-[78vh]">
+        <Outlet />
+      </main>
+      <Footer/>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+  <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white'>
+    Logo
+   </header>
+  )
+}
+
+export default Header
