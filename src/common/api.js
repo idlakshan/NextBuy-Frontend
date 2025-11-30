b49f518ba14e04e3a3bc1a473908e8a08fd36504
idlakshan <id.lakshan21@gmail.com>
2025-11-30 10:06:06 +0530

    feat: add register page

---
+++ src/common/api.js
@@ -0,0 +1,11 @@
+export const BaseURL="http://localhost:8080";
+
+const summaryApi={
+    register:{
+        url:"/api/user/register",
+        method:"POST",
+    }
+}
+
+
+export default summaryApi;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,164 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa6";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import Axios from "../utils/Axios";
+import summaryApi from "../common/api";
+import AxiosToastError from "../utils/AxiosToastError";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const navigate = useNavigate();
 
-export default Register
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const valideValue = Object.values(data).every((el) => el);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (data.password !== data.confirmPassword) {
+      toast.error("Password and Confrim Password must be the same");
+      return;
+    }
+
+    try {
+      const response = await Axios({
+        ...summaryApi.register,
+        data: data,
+      });
+
+      //  console.log(response);
+
+      if (response.data?.error) {
+        toast.error(response.data.message);
+      }
+
+      if (response.data?.success) {
+        toast.success(response.data.message);
+        setData({
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        });
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error?.response?.data?.message || error.message);
+      AxiosToastError(error);
+    }
+  };
+
+  return (
+    <section className="w-full container mx-auto px-2">
+      <div className="bg-white my-4 w-full max-w-lg mx-auto rounded p-7">
+        <p>Welcome to NextBuy</p>
+
+        <form className="grid gap-4 mt-6" onSubmit={handleSubmit}>
+          <div className="grid gap-1">
+            <label htmlFor="name">Name :</label>
+            <input
+              type="text"
+              id="name"
+              autoFocus
+              className="bg-gray-50 p-2 border rounded outline-none focus:border-primary-200"
+              name="name"
+              value={data.name}
+              onChange={handleChange}
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="email">Email :</label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 p-2 border rounded outline-none focus:border-primary-200"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="password">Password :</label>
+            <div className="bg-gray-50 p-2 border rounded flex items-center focus-within:border-primary-200">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                className="w-full outline-none"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                placeholder="Enter your password"
+              />
+              <div
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="cursor-pointer"
+              >
+                {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+              </div>
+            </div>
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="confirmPassword">Confirm Password :</label>
+            <div className="bg-gray-50 p-2 border rounded flex items-center focus-within:border-primary-200">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                className="w-full outline-none"
+                name="confirmPassword"
+                value={data.confirmPassword}
+                onChange={handleChange}
+                placeholder="Enter your confirm password"
+              />
+              <div
+                onClick={() => setShowConfirmPassword((prev) => !prev)}
+                className="cursor-pointer"
+              >
+                {showConfirmPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+              </div>
+            </div>
+          </div>
+
+          <button
+            disabled={!valideValue}
+            className={` ${
+              valideValue ? "bg-[#A3CB38] hover:bg-[#009432]" : "bg-gray-500"
+            }    text-white py-2 rounded font-semibold my-3 tracking-wide`}
+          >
+            Register
+          </button>
+        </form>
+
+        <p>
+          Already have account ?{" "}
+          <Link
+            to={"/login"}
+            className="font-semibold text-[#A3CB38]  hover:text-[#009432] "
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
